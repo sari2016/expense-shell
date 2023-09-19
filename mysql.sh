ba1aee0ec3999b750507dbf712d7cfd244729401
sarita1104 <saritagovindaraju83@gmail.com>
2023-09-18 23:00:35 -0500

    expense scripts

---
+++ mysql.sh
@@ -0,0 +1,8 @@
+dnf module disable mysql -y
+
+
+dnf install mysql-community-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql -uroot -pExpenseApp@1
