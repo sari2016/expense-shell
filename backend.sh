064019d1d3a053a73b8d1c94092b53c1388cc141
sarita1104 <saritagovindaraju83@gmail.com>
2023-09-21 10:04:38 -0500

    expense scripts

--- backend.sh
+++ backend.sh
@@ -2,58 +2,80 @@ source common.sh
 component=backend
 
 echo Install NodeJS Repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &&>>$log_file
 
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo FAILED
+    echo -e "\31mFAILED\e[0m"
     fi
 
-    echo Install NodeJS
-dnf install nodejs -y &>>$log_file
+echo Install NodeJS
+dnf install nodejs -y &&>>$log_file
+
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo FAILED
+    echo -e "\31mFAILED\e[0m"
     fi
 
     echo copy Backend Service File
-cp backend.service /etc/systemd/system/backend.service &>>$log_file
+cp backend.service /etc/systemd/system/backend.service &&>>$log_file
 
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo FAILED
+    echo -e "\31mFAILED\e[0m"
     fi
 
 echo Add Application User
-useradd expense &>>$log_file
+useradd expense &&>>$log_file
 if [ $? -eq 0 ]; then
-  echo SUCCESS
+  echo -e "\e[32mSUCCESS\e[0m"
   else
-    echo FAILED
+    echo -e "\31mFAILED\e[0m"
     fi
 echo Clean App Content
-rm -rf /app &>>$log_file
+rm -rf /app &&>>$log_file
 mkdir /app
 
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\31mFAILED\e[0m"
+    fi
 echo Download App Content
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &&>>$log_file
 cd /app
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\31mFAILED\e[0m"
+    fi
 echo Extract App Content
-unzip /tmp/backend.zip &>>$log_file
-
+unzip /tmp/backend.zip &&>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\31mFAILED\e[0m"
+    fi
 echo Download Dependencies
 cd /app
 npm install &>>$log_file
-
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\31mFAILED\e[0m"
+    fi
 echo Start Backend Service
-systemctl daemon-reload &>>$log_file
-systemctl enable backend &>>$log_file
-systemctl start backend &>>$log_file
-
+systemctl daemon-reload &&>>$log_file
+systemctl enable backend &&>>$log_file
+systemctl start backend &&>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\31mFAILED\e[0m"
+    fi
 echo Install MYSQL Client
 dnf install mysql -y
