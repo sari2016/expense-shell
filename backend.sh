8c3516ce82945f1e45816b1388bbf2191e51f6db
sarita1104 <saritagovindaraju83@gmail.com>
2023-09-21 10:51:30 -0500

    expense scripts

--- backend.sh
+++ backend.sh
@@ -2,50 +2,50 @@ source common.sh
 component=backend
 
 echo Install NodeJS Repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &&>>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
 stat_check
 
 
 
 echo Install NodeJS
-dnf install nodejs -y &&>>$log_file
+dnf install nodejs -y &>>$log_file
 stat_check
 
 
     echo copy Backend Service File
-cp backend.service /etc/systemd/system/backend.service &&>>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 stat_check
 
 echo Add Application User
-useradd expense &&>>$log_file
+useradd expense &>>$log_file
 stat_check
 
 echo Clean App Content
-rm -rf /app &&>>$log_file
+rm -rf /app &>>$log_file
 mkdir /app
 stat_check
 
 echo Download App Content
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &&>>$log_file
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app
 stat_check
 
 echo Extract App Content
-unzip /tmp/backend.zip &&>>$log_file
+unzip /tmp/backend.zip &>>$log_file
 
 stat_check
 
 echo Download Dependencies
 cd /app
-npm install &&>>$log_file
+npm install &>>$log_file
 stat_check
 
 echo Start Backend Service
-systemctl daemon-reload &&>>$log_file
-systemctl enable backend &&>>$log_file
-systemctl start backend &&>>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 stat_check
 echo Install MYSQL Client
 dnf install mysql -y
