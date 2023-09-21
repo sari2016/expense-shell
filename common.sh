c94ba17380ac0d9d6feb0c7577fc8d01a2331a67
sarita1104 <saritagovindaraju83@gmail.com>
2023-09-21 10:39:46 -0500

    expense scripts

--- common.sh
+++ common.sh
@@ -1,1 +1,13 @@
 log_file=/tmp/expense.log
+
+download_and_extract() {
+  echo Download $component Code
+  curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
+  echo $?
+
+  echo Extracting $component Code
+  unzip /tmp/$component.zip >>$lof_file
+  echo $?
+  }
+
+}
