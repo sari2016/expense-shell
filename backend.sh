1853e9bfa2cbef5bdc48e3ab57ff0f8665ac6ca9
sarita1104 <saritagovindaraju83@gmail.com>
2023-09-21 10:45:57 -0500

    expense scripts

--- backend.sh
+++ backend.sh
@@ -4,88 +4,48 @@ component=backend
 echo Install NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &&>>$log_file
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
+stat_check
+
+
 
 echo Install NodeJS
 dnf install nodejs -y &&>>$log_file
+stat_check
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
 
     echo copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &&>>$log_file
-
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
+stat_check
 
 echo Add Application User
 useradd expense &&>>$log_file
+stat_check
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
 echo Clean App Content
 rm -rf /app &&>>$log_file
 mkdir /app
+stat_check
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
 echo Download App Content
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &&>>$log_file
 cd /app
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
+stat_check
+
 echo Extract App Content
 unzip /tmp/backend.zip &&>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
+
+stat_check
+
 echo Download Dependencies
 cd /app
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
+stat_check
+
 echo Start Backend Service
 systemctl daemon-reload &&>>$log_file
 systemctl enable backend &&>>$log_file
 systemctl start backend &&>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILED\e[0m"
-    exit
-    fi
+stat_check
 echo Install MYSQL Client
 dnf install mysql -y
